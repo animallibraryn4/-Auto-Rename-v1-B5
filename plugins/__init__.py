203456f63fd6c540ba2b6c139dd83871773724c9
animallibraryn4 <nikhil7858978052@gmail.com>
2025-12-13 01:07:58 +0530

    Delete plugins/__init__.py

--- plugins/__init__.py
+++
@@ -1,9 +0,0 @@
-from plugins.file_rename import *
-from plugins.thumb_&_cap import *
-from plugins.quality_thumb import *
-from plugins.auto_rename import *
-from plugins.start_&_cb import *
-from plugins.force_subs import *
-from plugins.admin_panel import *
-from plugins.metadata import *
-from plugins.ban_system import *  # नया बैन सिस्टम
