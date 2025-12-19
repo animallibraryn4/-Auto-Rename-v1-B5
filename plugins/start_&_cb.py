6075c8037a817cf306225b065c0ca79503adbd28
animallibraryn4 <nikhil7858978052@gmail.com>
2025-12-19 14:12:41 +0530

    Update start_&_cb.py

--- plugins/start_&_cb.py
+++ plugins/start_&_cb.py
@@ -210,17 +210,6 @@ async def getpremium(bot, message):
     await yt.delete()
     await message.delete()
 
-# Plan Command Handler
-@Client.on_message(filters.command("plan"))
-async def premium(bot, message):
-    buttons = InlineKeyboardMarkup([
-        [InlineKeyboardButton("sᴇɴᴅ ss", url="https://t.me/Anime_library_n4"), InlineKeyboardButton("ᴄʟᴏsᴇ", callback_data="close")]
-    ])
-    yt = await message.reply_photo(photo='https://graph.org/file/8b50e21db819f296661b7.jpg', caption=Txt.PREPLANS_TXT, reply_markup=buttons)
-    await asyncio.sleep(300)
-    await yt.delete()
-    await message.delete()
-
 # Bought Command Handler
 @Client.on_message(filters.command("bought") & filters.private)
 async def bought(client, message):
@@ -255,5 +244,5 @@ async def help_command(client, message):
             [InlineKeyboardButton('ʜᴏᴍᴇ', callback_data='home')]
         ])
 )
-    
+
 
