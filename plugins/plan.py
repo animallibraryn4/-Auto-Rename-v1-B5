28733add81ef8e523308773da1e9efd62dfd7d47
animallibraryn4 <nikhil7858978052@gmail.com>
2025-12-20 02:05:23 +0530

    Update plan.py

--- plugins/plan.py
+++ plugins/plan.py
@@ -1,7 +1,10 @@
 import asyncio
 from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery, InputMediaPhoto
 from config import Txt, Config
+
+# --- Constants ---
+PLAN_PHOTO = "https://graph.org/file/8b50e21db819f296661b7.jpg"
 
 # --- Main Command ---
 @Client.on_message(filters.command("plan"))
@@ -12,8 +15,9 @@ async def plan_menu(bot, message):
         [InlineKeyboardButton("Pro", callback_data="view_pro"), InlineKeyboardButton("Ultra", callback_data="view_ultra")],
         [InlineKeyboardButton("Close", callback_data="close")]
     ]
-    await message.reply_text(
-        text=Txt.PLAN_MAIN_TXT.format(message.from_user.first_name),
+    await message.reply_photo(
+        photo=PLAN_PHOTO,
+        caption=Txt.PLAN_MAIN_TXT.format(message.from_user.first_name),
         reply_markup=InlineKeyboardMarkup(buttons)
     )
 
@@ -31,12 +35,15 @@ async def handle_callbacks(bot, query: CallbackQuery):
             [InlineKeyboardButton("Pro", callback_data="view_pro"), InlineKeyboardButton("Ultra", callback_data="view_ultra")],
             [InlineKeyboardButton("Close", callback_data="close")]
         ]
-        await query.message.edit_text(Txt.PLAN_MAIN_TXT.format(user_name), reply_markup=InlineKeyboardMarkup(buttons))
+        await query.message.edit_message_caption(
+            caption=Txt.PLAN_MAIN_TXT.format(user_name), 
+            reply_markup=InlineKeyboardMarkup(buttons)
+        )
 
     elif data.startswith("view_"):
         plan_type = data.split("_")[1]
         
-        # Define Plan Content
+        # Define Plan Content: (Text, Page, Prev, Next, Pay_Callback)
         plans = {
             "free": (Txt.FREE_TXT, "1/6", "view_ultra", "view_basic", None),
             "basic": (Txt.BASIC_TXT, "2/6", "view_free", "view_lite", "pay_basic"),
@@ -54,19 +61,29 @@ async def handle_callbacks(bot, query: CallbackQuery):
         elif plan_type == "free":
             btn.append([InlineKeyboardButton("Admit Link", url="https://t.me/Anime_Library_N4")])
             
-        btn.append([InlineKeyboardButton("Back", callback_data=prev_p), InlineKeyboardButton(page, callback_data="none"), InlineKeyboardButton("Next", callback_data=next_p)])
+        btn.append([
+            InlineKeyboardButton("Back", callback_data=prev_p), 
+            InlineKeyboardButton(page, callback_data="none"), 
+            InlineKeyboardButton("Next", callback_data=next_p)
+        ])
         btn.append([InlineKeyboardButton("Back to Menu", callback_data="main_plan")])
         
-        await query.message.edit_text(txt.format(user_name), reply_markup=InlineKeyboardMarkup(btn))
+        await query.message.edit_message_caption(
+            caption=txt.format(user_name), 
+            reply_markup=InlineKeyboardMarkup(btn)
+        )
 
     elif data.startswith("pay_"):
-        plan_origin = data.split("_")[1] # basic, lite, etc.
+        plan_origin = data.split("_")[1]
         buttons = [
             [InlineKeyboardButton("Pay via UPI ID", callback_data=f"upi_{plan_origin}")],
             [InlineKeyboardButton("Scan QR Code", callback_data=f"qr_{plan_origin}")],
             [InlineKeyboardButton("Back", callback_data=f"view_{plan_origin}")]
         ]
-        await query.message.edit_text(Txt.SELECT_PAYMENT_TXT, reply_markup=InlineKeyboardMarkup(buttons))
+        await query.message.edit_message_caption(
+            caption=Txt.SELECT_PAYMENT_TXT, 
+            reply_markup=InlineKeyboardMarkup(buttons)
+        )
 
     elif data.startswith("upi_") or data.startswith("qr_"):
         method, origin = data.split("_")
@@ -75,10 +92,11 @@ async def handle_callbacks(bot, query: CallbackQuery):
             [InlineKeyboardButton("Send payment screenshot here", url="https://t.me/Animelibraryn4")],
             [InlineKeyboardButton("Back", callback_data=f"pay_{origin}")]
         ]
-        await query.message.edit_text(txt.format(user_name), reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=False)
+        await query.message.edit_message_caption(
+            caption=txt.format(user_name), 
+            reply_markup=InlineKeyboardMarkup(buttons)
+        )
 
     elif data == "close":
         await query.message.delete()
-        try: await query.message.reply_to_message.delete()
-        except: pass
-
+        
