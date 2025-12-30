1fa4acf145b857a43163e29afd70f465d83161e4
animallibraryn4 <nikhil7858978052@gmail.com>
2025-12-30 21:44:52 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -30,7 +30,7 @@ class Bot(Client):
 
             workers=50,
             plugins={"root": "plugins"},
-            sleep_threshold=60,
+            sleep_threshold=120,
         )
 
         self.start_time = time.time()
